7a1284ecca70ffd1a91ddcd06cc15194cd59ef51
Mervin Gaitho <gmel83970@gmail.com>
2025-12-08 16:11:50 +0000

    Update routing and enhance BaseController: Changed the main route to point to Alphawonders and added multiple new routes for various functionalities. Expanded BaseController with detailed documentation and improved request handling to support future controller development.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,19 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Alphawonders::index');
+$routes->get('/home', 'Alphawonders::home');
+$routes->get('/softwares', 'Alphawonders::alphasoftwares');
+$routes->get('/system-administration', 'Alphawonders::alphasystems');
+$routes->get('/design', 'Alphawonders::alphadesign');
+$routes->get('/digital-marketing', 'Alphawonders::alphamarketing');
+$routes->get('/ict-consultancy', 'Alphawonders::alphaconsultancy');
+$routes->get('/it-support', 'Alphawonders::alphasupport');
+$routes->get('/hire', 'Alphawonders::alphahires');
+$routes->get('/contact-us', 'Alphawonders::contact');
+$routes->post('/contact-us', 'Alphawonders::send_contact_data');
+$routes->post('/subscribe', 'Alphawonders::subscriptions_email');
+$routes->post('/hire-submit', 'Alphawonders::hires_details');
+$routes->get('/blog', 'Alphawonders::alphablog');
+$routes->get('/blog/post', 'Alphawonders::alphapost');
+$routes->post('/blog/comment', 'Alphawonders::post_comments');
