2ab67430dd8514122d5c03e73e76e881351d07d6
Mervin Gaitho <gmel83970@gmail.com>
2025-12-08 15:49:37 +0000

    Refactor error handling views: Removed outdated error handling files and replaced them with new PHP and HTML templates for better error reporting. Introduced a production error page and updated existing error templates to improve user experience and maintainability.

--- app/Views/errors/cli/error_404.php
+++ app/Views/errors/cli/error_404.php
@@ -1,7 +1,7 @@
-export default function absRound (number) {
-    if (number < 0) {
-        return Math.round(-1 * number) * -1;
-    } else {
-        return Math.round(number);
-    }
-}
+<?php
+
+use CodeIgniter\CLI\CLI;
+
+CLI::error('ERROR: ' . $code);
+CLI::write($message);
+CLI::newLine();
